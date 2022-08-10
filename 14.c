d48a93f39f0e09000811c22d226a866dc3c901ae
Lloyd022 <hendrickmalatji017@gmail.com>
2022-08-10 03:18:33 -0700

    14 changes

--- 14.c
+++ 14.c
@@ -1,23 +1,36 @@
 #include "main.h"
-
 /**
-*  * printf_srev - function that prints a str in reverse
+*  * printf_rot13 - printf str to ROT13 place into buffer
 *   * @args: type struct va_arg where is allocated printf arguments
-*    *
-*     * Return: the string
+*    * Return: counter
 */
-int printf_sav(va_list args)
+int printf_rot13(va_list args)
 {
-
+int i, j, counter = 0;
+int k = 0;
 char *s = va_arg(args, char*);
-int i;
-int j = 0;
+char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
 
 if (s == NULL)
 s = "(null)";
-while (s[j] != '\0')
-j++;
-for (i = j - 1; i >= 0; i--)
+for (i = 0; s[i]; i++)
+{
+k = 0;
+for (j = 0; alpha[j] && !k; j++)
+{
+if (s[i] == alpha[j])
+{
+_putchar(beta[j]);
+counter++;
+k = 1;
+}
+}
+if (!k)
+{
 _putchar(s[i]);
-return (j);
+counter++;
+}
+}
+return (counter);
 }
